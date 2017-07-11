9dabc2bfbc90f8a2494d2f75f01c4d5b34d579af
harshaghub <harshaghub>
2017-07-10 22:41:53 -0600

    else file updated

--- else.py
+++ else.py
@@ -1,6 +1,6 @@
 distance=input('how far would you like to travel in miles?:')
 distance=float(distance)
-if distance<3:
+if distance<4:
     mode_of_transport='walking'
 elif distance<300:
     mode_of_transport='driving'
